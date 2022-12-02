3ef5b92e06872d5816b75fc0cc6f6c7fbf66e2ac
narfnra <narfnra@nidhoggr>
2022-12-02 05:19:22 -0600

    Improve Gym Leader/E4 Movesets.

--- data/trainers/special_moves.asm
+++ data/trainers/special_moves.asm
@@ -14,73 +14,104 @@ SpecialTrainerMoves:
 	db 0
 
 	db BROCK, 1
+	db 1, 1, RAGE
+	db 2, 1, ROCK_THROW
+	db 2, 2, REST
 	db 2, 3, BIND
 	db 2, 4, BIDE
 	db 0
 
 	db MISTY, 1
-	db 2, 4, BUBBLEBEAM
+	db 1, 3, MINIMIZE
+	db 1, 4, LIGHT_SCREEN
+	db 2, 1, TRI_ATTACK
+	db 2, 2, BUBBLEBEAM
 	db 0
 
 	db LT_SURGE, 1
-	db 1, 1, THUNDERBOLT
-	db 1, 2, MEGA_PUNCH
-	db 1, 3, MEGA_KICK
-	db 1, 4, GROWL
+	db 1, 1, TAKE_DOWN
+	db 1, 4, SELFDESTRUCT
+	db 3, 1, THUNDERBOLT
+	db 3, 2, SUBMISSION
+	db 3, 3, MEGA_KICK
+	db 3, 4, FLY
 	db 0
 
 	db ERIKA, 1
-	db 1, 3, MEGA_DRAIN
-	db 2, 1, RAZOR_LEAF
-	db 3, 1, PETAL_DANCE
+	db 1, 3, ACID
+	db 2, 1, CUT
+	db 2, 3, MEGA_DRAIN
+	db 2, 4, SWORDS_DANCE
+	db 3, 2, BODY_SLAM
+	db 3, 3, MEGA_DRAIN
 	db 0
 
 	db KOGA, 1
-	db 1, 1, TOXIC
-	db 1, 2, TACKLE
-	db 2, 1, TOXIC
-	db 2, 3, SUPERSONIC
-	db 3, 1, TOXIC
-	db 3, 2, DOUBLE_EDGE
-	db 4, 1, LEECH_LIFE
-	db 4, 2, DOUBLE_TEAM
-	db 4, 3, PSYCHIC_M
-	db 4, 4, TOXIC
+	db 1, 1, EXPLOSION
+	db 1, 2, TOXIC
+	db 2, 2, TOXIC
+	db 2, 3, MEGA_DRAIN
+	db 3, 1, THUNDERBOLT
+	db 3, 2, TOXIC
+	db 4, 1, FIRE_BLAST
+	db 4, 3, TOXIC
+	db 4, 4, THUNDERBOLT
 	db 0
 
 	db BLAINE, 1
 	db 1, 1, FLAMETHROWER
-	db 1, 4, CONFUSE_RAY
-	db 3, 1, FLAMETHROWER
-	db 3, 2, FIRE_BLAST
-	db 3, 3, REFLECT
+	db 2, 1, CONFUSE_RAY
+	db 2, 2, SWIFT
+	db 2, 3, REFLECT
+	db 3, 1, HORN_DRILL
+	db 3, 3, FIRE_BLAST
+	db 4, 1, AGILITY
+	db 4, 2, DIG
+	db 4, 3, FIRE_BLAST
 	db 0
 
 	db SABRINA, 1
-	db 1, 1, FLASH
-	db 2, 1, KINESIS
-	db 2, 4, PSYWAVE
+	db 1, 1, COUNTER
+	db 1, 2, REFLECT
+	db 2, 4, METRONOME
 	db 3, 1, PSYWAVE
+	db 3, 2, TOXIC
+	db 3, 4, PSYCHIC_M
+	db 4, 1, DIG
+	db 4, 3, TRI_ATTACK
 	db 0
 
 	db GIOVANNI, 3
-	db 1, 3, FISSURE
-	db 2, 2, DOUBLE_TEAM
+	db 1, 1, SKULL_BASH
+	db 1, 2, COUNTER
+	db 1, 3, SUBSTITUTE
+	db 1, 4, ROCK_SLIDE
+	db 2, 1, FISSURE
+	db 2, 3, ROCK_SLIDE
 	db 3, 1, EARTHQUAKE
-	db 3, 3, THUNDER
+	db 3, 2, BLIZZARD
+	db 3, 3, SURF
+	db 3, 3, FIRE_BLAST
 	db 4, 1, EARTHQUAKE
-	db 4, 2, LEER
-	db 4, 3, THUNDER
-	db 5, 1, ROCK_SLIDE
-	db 5, 4, EARTHQUAKE
+	db 4, 2, THUNDER
+	db 4, 3, BUBBLEBEAM
+	db 5, 1, EARTHQUAKE
+	db 5, 2, ROCK_SLIDE
+	db 5, 3, SUBMISSION
+	db 5, 4, BODY_SLAM
 	db 0
 
 	db LORELEI, 1
 	db 1, 1, BUBBLEBEAM
+	db 1, 2, ICE_BEAM
+	db 2, 1, EXPLOSION
 	db 2, 3, ICE_BEAM
+	db 2, 4, TRI_ATTACK
 	db 3, 1, PSYCHIC_M
-	db 3, 2, SURF
+	db 3, 2, REST
+	db 4, 1, PSYCHIC_M
 	db 4, 3, LOVELY_KISS
+	db 4, 4, SUBSTITUTE
 	db 5, 3, BLIZZARD
 	db 0
 
@@ -88,27 +119,29 @@ SpecialTrainerMoves:
 	db 1, 1, ROCK_SLIDE
 	db 1, 2, SCREECH
 	db 1, 4, DIG
-	db 2, 3, FIRE_PUNCH
-	db 2, 4, DOUBLE_TEAM
-	db 3, 1, DOUBLE_KICK
-	db 3, 2, MEGA_KICK
-	db 3, 4, DOUBLE_TEAM
+	db 2, 2, SUBMISSION
+	db 2, 4, AGILITY
+	db 3, 1, MEDITATE
+	db 3, 2, METRONOME
 	db 4, 1, ROCK_SLIDE
 	db 4, 2, SCREECH
 	db 4, 4, EARTHQUAKE
-	db 5, 2, KARATE_CHOP
-	db 5, 3, STRENGTH
+	db 5, 1, HYPER_BEAM
+	db 5, 2, STRENGTH
+	db 5, 3, ROCK_SLIDE
+	db 5, 4, SUBMISSION
 	db 0
 
 	db AGATHA, 1
-	db 1, 2, SUBSTITUTE
-	db 1, 3, LICK
+	db 1, 1, EXPLOSION
+	db 1, 2, THUNDER
 	db 1, 4, MEGA_DRAIN
-	db 2, 2, TOXIC
-	db 2, 4, LEECH_LIFE
-	db 3, 2, LICK
-	db 4, 1, WRAP
+	db 2, 1, DOUBLE_EDGE
+	db 2, 3, TOXIC
+	db 2, 4, DOUBLE_TEAM
+	db 4, 1, EARTHQUAKE
 	db 5, 2, PSYCHIC_M
+	db 5, 4, THUNDERBOLT
 	db 0
 
 	db LANCE, 1
